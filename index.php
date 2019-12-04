26e768a04f22af92f72d584d8ebacf93613b45c1
GnatJ <57310641+GnatJ@users.noreply.github.com>
2019-12-04 18:51:58 +0200

    everything

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Gina Cooks</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	
+
+</head>
+<body>
+	<!-- SITE HEADER -->
+	<header>
+		<!-- <div>
+			<img src="images/spices.jpg" alt="header image of spices" width="100%" class="headimage">
+			<h1 class="SiteName"><a href="index.html">Gina Cooks</a></h1>
+		</div>
+
+						
+		<div id="navigation">
+			<nav>
+				<ul>
+					<li><a id="highlight" href="index.html">Home</a></li>
+					<li><a href="about.html">About</a></li>
+					<li><a href="recipes.html">Recipes</a></li>
+					<li><a href="contact.html">Contact</a></li>
+				</ul>
+			</nav>
+		</div>
+
+		<div id="clear"></div> -->
+		
+		<? include "nav.php"; ?>
+
+	</header> 
+
+	
+
+	<!-- END SITE HEADER -->
+
+	<!-- MAIN BODY -->
+	<div id="RecipeMonth">
+		<h2>Recipe of the Month</h2>
+	</div>
+
+	<!-- END MAIN BODY -->
+
+	
+
+</body>
+</html>
---
+++ nav.php
@@ -0,0 +1,23 @@
+	<?php
+	echo "
+		<div>
+			<img src="images/spices.jpg" alt="header image of spices" width="100%" class="headimage">
+			<h1 class="SiteName"><a href="index.html">Gina Cooks</a></h1>
+		</div>
+
+						
+		<div id="navigation">
+			<nav>
+				<ul>
+					<li><a id="highlight" href="index.html">Home</a></li>
+					<li><a href="about.html">About</a></li>
+					<li><a href="recipes.html">Recipes</a></li>
+					<li><a href="contact.html">Contact</a></li>
+				</ul>
+			</nav>
+		</div>
+
+		<div id="clear"></div>
+
+		";
+	?>
